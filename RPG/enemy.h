543f89e37b321dc13cad8c0992633c61e0915e49
Omar Abed <omarabed@vt.edu>
2013-04-15 09:56:54 -0400

    added num gen files and updated enemy.h

--- RPG/enemy.h
+++ RPG/enemy.h
@@ -1,9 +1,34 @@
-#ifndef ENEMY_H
-#define ENEMY_H
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 class Enemy
 {
-};
+private:
+	int level;
 
-#endif // ENEMY_H
+	double health;
+	double maxHealth;
+
+	double agil;		// agility
+	double dam;		// Damage
+	double sDam;		// Special Damage
+	double armor;		// Armor
+	double exp;		// Experience
+
+	string name;
+	string attack;
+	string sAttack;
+public:
+
+Enemy(int elvl);
+
+void setAttack();
+string getAttack();
+
+void attack();			// 
+void specAtt();			// Special Attack
+
+void restore();			// restores health
+}
